52f5eaa3b86d03a98c0361a2ab38e3c724ff6088
Vasil Kotsev <Vasko1223s@gmail.com>
2018-11-21 19:09:22 +0200

    Added exercise

---
+++ SOLID/Exercise/SOLID-Exercise/Logger/Layouts/Contracts/ILayout.cs
@@ -0,0 +1,10 @@
+﻿// Filename: ILayout.cs
+// Author: Vasil K.
+// Date: 18:32:21  11/21/2018
+namespace Logger.Layouts.Contracts
+{
+    public interface ILayout
+    {
+        string Format { get; }
+    }
+}
---
+++ SOLID/Exercise/SOLID-Exercise/Logger/Layouts/SimpleLayout.cs
@@ -0,0 +1,15 @@
+﻿// Filename: SimpleLayout.cs
+// Author: Vasil K.
+// Date: 18:35:56  11/21/2018
+namespace Logger.Layouts
+{
+    using Contracts;
+
+    public class SimpleLayout : ILayout
+    {
+        public string Format
+        {
+            get { return "{0} - {1} - {2}"; }
+        }
+    }
+}
